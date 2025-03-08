d4734e71b4ef07f0d5190107d15a1db7b27d78e6
PenAndPapers <steven.pilagan@gmail.com>
2025-03-08 12:46:44 +0800

    added comments

--- app/modules/user/router.py
+++ app/modules/user/router.py
@@ -6,22 +6,22 @@ from .providers import get_user_service
 
 router = APIRouter(prefix="/user", tags=["User"])
 
-
+# TODO check if user is authenticated
 @router.post("", response_model=UserResponse)
 def create(data: UserCreate, service: UserService = Depends(get_user_service)):
     pass
 
-
+# TODO check if user is authenticated
 @router.get("/all", response_model=List[UserResponse])
 def get_all(service: UserService = Depends(get_user_service)):
     pass
 
-
+# TODO check if user is authenticated
 @router.get("/{id}", response_model=UserResponse)
 def get_by_id(id: int, service: UserService = Depends(get_user_service)):
     pass
 
-
+# TODO check if user is authenticated
 @router.patch("/{id}", response_model=UserResponse)
 def update(
     id: int,
@@ -30,7 +30,7 @@ def update(
 ):
     pass
 
-
+# TODO check if user is authenticated
 @router.delete("/{id}")
 def delete(id: int, service: UserService = Depends(get_user_service)):
     pass
